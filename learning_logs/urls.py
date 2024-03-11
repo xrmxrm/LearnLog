282f306a6b7b4f48ed95716684d2a4df88fef128
richard <xrmxrm@gmail.com>
2024-03-10 22:53:16 -0700

    Add a basic form

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     # Detail page for a single topic.
     path('topics/<int:topic_id>/', views.topic, name='topic'),
 
-
+    # Page for adding a new topic.
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
