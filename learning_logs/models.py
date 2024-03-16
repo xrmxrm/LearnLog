b11345e85f2c16f4e71990cd35201e7c6a9ec6e2
richard <xrmxrm@gmail.com>
2024-03-16 15:41:41 -0700

    Apply owner attribute to topic, not entry

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -5,6 +5,8 @@ class Topic(models.Model):
     """A topic the user is learning about"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
     def __str__(self):
         """Return a string representation of the model"""
@@ -15,7 +17,6 @@ class Entry(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = 'entries'
