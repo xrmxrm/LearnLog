f6af04d8aaecb21238178be7b0ff7df9276d3554
richard <xrmxrm@gmail.com>
2024-03-16 15:32:05 -0700

    Add owner to model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Topic(models.Model):
     """A topic the user is learning about"""
@@ -14,6 +15,7 @@ class Entry(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = 'entries'
