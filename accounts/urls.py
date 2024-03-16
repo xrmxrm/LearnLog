5b402d91f39135b83070bef5e666944a896cd8f5
richard <xrmxrm@gmail.com>
2024-03-16 11:00:58 -0700

    Build logout

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+"""URL patterns for accounts"""
+
+from django.urls import path, include
+
+app_name = 'accounts'
+urlpatterns = [
+    # Include default auth urls.
+    path('', include('django.contrib.auth.urls')),
+]
--- ll_project/settings.py
+++ ll_project/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My settings.
+LOGIN_REDIRECT_URL = 'learning_logs:index'
+LOGOUT_REDIRECT_URL = 'learning_logs:index'
